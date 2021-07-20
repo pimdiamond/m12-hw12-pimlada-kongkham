1be9727a2a939ccdffe97e531ae47438e2e255f0
Pimlada <pimdiamond@gmail.com>
2021-07-20 17:39:36 -0400

    modified js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const places = ["Alaska","Greece", "Iceland", "Japan", "Norway", "Antarctica", "Kukenan River", "Lake Urmia","Parc de la Vanoise", "Banff National Park",];
-document.getElementById("list").innerHTML = places;  
+document.getElementById("list").innerHTML = places[0];
 
 function myFunction() {
   for (let i = places.length -1; i > 0; i--) {
@@ -8,7 +8,7 @@ function myFunction() {
     places[i] = places[j]
     places[j] = k
   }
-  document.getElementById("list").innerHTML = places;
+  document.getElementById("list").innerHTML = places[0];
 }
 
 
